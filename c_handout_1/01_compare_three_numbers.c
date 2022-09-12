ae5d96410987dd6d5487e912e5126fe9d9a568dd
Evin Hsu <yiyun1429@gmail.com>
2022-09-12 21:37:25 +0800

    Create 01_compare_three_numbers.c

---
+++ c_handout_1/01_compare_three_numbers.c
@@ -0,0 +1,40 @@
+/* 
+** 輸入三個整數 a、b、c，印出此三值的最大值、最小值、總和及平均值
+** Usage : $ main int1 int2 int3
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc < 4){
+        printf("Usage: \"main int1 int2 int3\"");
+        return -1;
+    }
+    
+    int a, b ,c;
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    c = atoi(argv[3]);
+    
+    printf("Got number: %d, %d, %d.\n", a, b, c);
+    
+    int temp = 0;
+    // max number
+    temp = (a >= b)? a : b;
+    temp = (c >= temp)? c: temp;
+    printf("Max: %d\n", temp);
+    // min number
+    temp = (a <= b)? a : b;
+    temp = (c <= temp)? c: temp;
+    printf("Min: %d\n", temp);
+    // summation
+    temp = a + b + c;
+    printf("Sum: %d\n", temp);
+    // average
+    temp = temp / 3;
+    printf("Average: %d\n", temp);
+    return 0;
+}
