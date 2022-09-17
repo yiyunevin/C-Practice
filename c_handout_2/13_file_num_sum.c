f88fa8c76adcf1d671ceac88acf1ad179dd6d7af
Evin Hsu <yiyun1429@gmail.com>
2022-09-17 14:01:33 +0800

    Update and rename f_num_sum.c to 13_file_num_sum.c

---
+++ c_handout_2/13_file_num_sum.c
@@ -0,0 +1,34 @@
+/* 由命令列指定文字檔名稱，讀取一串以空白間隔之數字，印出數字數量與總和
+** ------------------------------------------------------------------
+** * Usage ./13_file_num_sum f_num_sum.txt 
+** ------------------------------------------------------------------
+** * read text file
+** * stdlib.h / exit(1)
+** * read file seperated by space
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main( int argc, char *argv[] )
+{
+  int count = 0, sum = 0;
+  // open file
+  FILE* fp;
+  fp = fopen(argv[1], "r");
+  if(fp == NULL){
+    printf("File not exist.\n");
+    exit(1);
+  }
+  // read file
+  int n;
+  while((fscanf(fp, "%d", &n)) != EOF){
+    if(!(n==' ')){
+      count ++;
+      sum += n;
+    }
+  }
+  fclose(fp);
+  printf("Ans = %d, %d.\n", count, sum);
+  return 0;
+}
