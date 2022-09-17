6c7d3ce8c54878b2b7721a6f83671bcdd876da78
Evin Hsu <yiyun1429@gmail.com>
2022-09-17 22:27:35 +0800

    update

--- c_handout_2/14-1_swap_accumulate.c
+++ c_handout_2/14-1_swap_accumulate.c
@@ -1,3 +1,14 @@
+/* scanf 輸入兩整數 a b:
+** STEP 1 : 撰寫一函式，若 a>b 則對調兩數 (return void)
+** STEP 2 : 由 1 累加到 b
+** ------------------------------------------------------------
+** Enter two integer: 8 2
+** 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36
+** ------------------------------------------------------------
+** * call by reference
+** * static
+*/
+
 #include<stdio.h>
 
 void swap(int *a, int *b)
@@ -19,6 +30,7 @@ int accum(int b)
 	}
 	return 1;
 }
+
 int main()
 {
 	//read value
