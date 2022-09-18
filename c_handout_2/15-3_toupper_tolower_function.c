979d4af5729b4a107ae6022b34119f4115810b93
Evin Hsu <yiyun1429@gmail.com>
2022-09-18 20:48:17 +0800

    Update and rename 15-5_toupper_tolower_function.c to 15-3_toupper_tolower_function.c

--- c_handout_2/15-3_toupper_tolower_function.c
+++ c_handout_2/15-3_toupper_tolower_function.c
@@ -1,3 +1,13 @@
+/* 由鍵盤輸入一長度小於 21 的字串 str1，若超過則印出字串與長度，並退出程式。否則將小寫改為大寫、大寫改為小寫並輸出（toupper(char)、tolower(char)）。使用函式 void transform(char *str1, char *str2) 完成 
+** ---------------------------------------------------------
+** Enter String: "Hello World!"
+** -> "hELLO wORLD!"
+** ---------------------------------------------------------
+** * upper / lower (ctype.h)
+** * input string with space
+** * one-dimension array call by reference
+*/
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
