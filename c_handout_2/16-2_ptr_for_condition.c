aa53621aa519fb2ff5d7ff6f5a9428b2f15abef1
Evin Hsu <yiyun1429@gmail.com>
2022-09-26 23:07:35 +0800

    Update 16-2_ptr_for_condition.c

--- c_handout_2/16-2_ptr_for_condition.c
+++ c_handout_2/16-2_ptr_for_condition.c
@@ -1,7 +1,16 @@
 /* 依序印出由第 x 個到最後的陣列元素數值與位址，for 迴圈的「起始值、條件判斷、增減量」都必須使用指向陣列的指標 ptr
 ** ----------------------------------------------------------------------------------------------------------
+** Enter an Interger 0-9 : 7
+** [value] ptr + 7 = 17
+** [addr]  ptr + 7 = (unsigned int) 4294954012     (hexadecimal)ffffcc1c   (void ptr)0xffffcc1c
+** [value] ptr + 8 = 18
+** [addr]  ptr + 8 = (unsigned int) 4294954016     (hexadecimal)ffffcc20   (void ptr)0xffffcc20
+** [value] ptr + 9 = 19
+** [addr]  ptr + 9 = (unsigned int) 4294954020     (hexadecimal)ffffcc24   (void ptr)0xffffcc24
+** -----------------------------------------------------------------------------------------------------------
 ** arithmetic and comparison of pointers
 ** print address
+** address of array
 */
 
 #include<stdio.h>
