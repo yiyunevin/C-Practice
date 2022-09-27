ac6be326661df4cac0bfb48375ffcf84f37c784c
Evin Hsu <yiyun1429@gmail.com>
2022-09-27 14:06:08 +0800

    Update 19-2_tokenpast.c

--- c_handout_2/19-2_tokenpast.c
+++ c_handout_2/19-2_tokenpast.c
@@ -1,5 +1,12 @@
+/* 定義 tokenpast(N) 以 token pasting 預處理運算子組合 token 與 n，宣告 int token 1 = 10，token 2 = 11，token 3 = 21，執行 tokenpast 印出 tokenN * 2 = x（由__DATE__取得的今日日期），N 僅限 1~3，否則輸出「Only 1 2 3」並結束程式。
+** --------------------------------------------------------------------------------
+** token pasting
+** preprocessor
+*/
+
 #include<stdio.h>
 
+// stringize and token-pasting
 # define tokenpast(N)   \
     printf("\ntoken" #N " * 2 = %d. %s\n", token##N * 2, __DATE__)
 
@@ -11,7 +18,8 @@ int main(){
     int token1 = 10;
     int token2 = 11;
     int token3 = 21;
-
+    
+    // preprocessor cannot use variables that won't be defined until runtime
     switch(n){
         case 1:
             tokenpast(1);
