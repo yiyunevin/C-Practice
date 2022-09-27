7abd225cb2c781a74676d79b1e78d8b241c39fc1
Evin Hsu <yiyun1429@gmail.com>
2022-09-27 14:03:25 +0800

    Update 19-1_stringize.c

--- c_handout_2/19-1_stringize.c
+++ c_handout_2/19-1_stringize.c
@@ -1,3 +1,11 @@
+/* 定義 happy_for(name)，輸出「Happy name's Day! 月  日  年」並以 stringize 預處理運算子轉換參數
+** 若輸入等於 5 則 name 為 Mother，8 則為 Father。日期則使用預定義巨集 __DATE__。
+** ------------------------------------------------------------------------------------------------------
+** * macro continuation (\)
+** * preprocessor stringize (#)
+** * 
+*/
+
 #include<stdio.h>
 
 #define happy_for(name) \
